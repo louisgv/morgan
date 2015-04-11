d2060d34478015c0eb8903d02171b41449b975f3
Ross Snider <ross.snider@gmail.com>
2015-04-11 14:36:05 -0700

    More modifications to the map and narration objects

---
+++ content/Blog_files/ajs.php
@@ -0,0 +1,7 @@
+(function(){
+var s = "<"+"script type=\"text/javascript\" src=\"http://a440.casalemedia.com/pcreative?au=4&c=2081C4&pcid=C8A820052D00&pr=55&r=C8A82005&s=25014&t=55298BCC&u=VlFweDFNQW9JajhBQUhRZU4yc0FBQUNV&m=d86115a35a01747489934eb15b240923&wp=FA&cp=1.018&aid=AC7E1180DC5C4DA7&tid=0&dm=64&n=www.dailykos.com\"><"+"/script>\n";
+s += "<"+"div id=\'beacon_cd80d70fa4\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"http://cat.sv.us.criteo.com/delivery/lg.php?cppv=1&cpp=m0S3eHw5bnp3Ull4TGYwRGQvT1BFZTUyMDVCc0FpMHRMR2lpMGcrbW9vSWFWTzFOdnFXeG5VTTFiYnJhSkVmbzZtNDZ5UEVzTGJsWWNBZzc5REx6T1huT3pDQzRhcWF0RjZ1ZkNsZmF6QzVMSUlXMkxtaktpcTNzdldGNkNQT3liVnJFdGJjNFVBTHF6bi9iSVBNYUhGanpZYnVyZThCbEVOMS9SVUI1eHFNbnV5LzBZLzNQUnRaU0pWQjd5VXNodEx2UHZ5eUJGMHFWL3NRTHp3d256TXRJd2RRNXpaNzdqanZEZUE5Q3c3YkxlOVE4PXw%3D\"/>\n";
+s += "<"+"/div>\n";
+s += "\n";
+document.write(s);})();
