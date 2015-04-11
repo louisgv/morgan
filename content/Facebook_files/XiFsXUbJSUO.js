e4f24b2e1762ad4c216ecc1f64ed5be768f4aad7
Ross Snider <ross.snider@gmail.com>
2015-04-11 00:19:35 -0700

    Added base node.js server

---
+++ content/Facebook_files/XiFsXUbJSUO.js
@@ -0,0 +1,5 @@
+/*!CK:1243114019!*//*1428703892,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["CcHQx"]); }
+
+__d("ShareNowCounter",["AsyncSignal","XShareNowCounterController"],function(a,b,c,d,e,f,g,h){b.__markCompiled&&b.__markCompiled();var i={logEvent:function(event){var j=h.getURIBuilder().setEnum('event',event).getURI();(new g(j)).send();}};e.exports=i;},null);
