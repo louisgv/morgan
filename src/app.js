cbe759fa39a870ee85e83dc3bf955571d928646b
Ross Snider <ross.snider@gmail.com>
2015-04-11 00:47:10 -0700

    Basic node set up with express routing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
-var app = require('express');
+var express = require('express'),
+    session = require('express-session');
 
-console.log("Hello World");
+var app = express();
+app.listen(1337);
+
+app.use(session({secret : "Morgan Freeman is a snowman who plays the trumpet"}));
+app.use(express.static('content'));
+
+app.get('/facebook', function (req, res) {
+    console.log(1);
+    res.sendfile('Facebook.html', {root : 'content'});
+});
+
+app.get('/fake', function (req, res) {
+  res.json({message : "This is a fake message"});
+});
+
+module.exports = app;
