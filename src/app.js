49741cfc1d83b021ef4a148a706f64ff72ed5988
Ross Snider <ross.snider@gmail.com>
2015-04-11 07:53:04 -0700

    Start and end page, content replacement and some new content examples

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    session = require('express-session');
+    session = require('express-session'),
+    fs      = require('fs');
 
 var app = express();
 app.listen(1337);
@@ -10,9 +11,9 @@ app.use(express.static('content'));
 app.use('/node', express.static('content'));
 
 var map = {
-    'start' : { 0 : 'dispa', 1 : 'dispa', 2 : 'dispa', file : "Facebook.html" },
-    'dispa' : { 0 : 'strat', 1 : 'strat', 2 : 'strat', file : "Facebook.html" },
-    'strat' : {	0 : 'event', 1 : 'event', 2 : 'event', file : "Facebook.html" },
+    'start' : { 0 : 'dispa', 1 : 'dispa', 2 : 'dispa', file : "start.html" },
+    'dispa' : { 0 : 'strat', 1 : 'strat', 2 : 'strat', file : "Facebook.html", zed : "Test comment number 1", one : "Test comment number 2", two : "Test comment number 3" },
+    'strat' : {	0 : 'event', 1 : 'event', 2 : 'event', file : "Twitter.html" },
     'event' : {	0 : 'nlike', 1 : 'ylike', 2 : 'nlike', file : "Facebook.html" },
     'nlike' : { 0 : 'sprot', 1 : 'sprot', 2 : 'sprot', file : "Facebook.html" },
     'ylike' : { 0 : 'sprot', 1 : 'sprot', 2 : 'sprot', file : "Facebook.html" },
@@ -32,21 +33,54 @@ var map = {
     '_exit' : { 0 : '_exit', 1 : '_exit', 2 : '_exit', file : "Facebook.html" },
 }
 
+var narrative = {
+    'start' : 'This is the start',
+    'dispa' : 'There is a lot of disparity',
+    'strat' : 'Stratification is high',
+    'event' : 'Shocking events happen',
+    'nlike' : 'Boo, this is lame',
+    'ylike' : 'Finally time that the news shows this happening',
+    'sprot' : 'Protestors are gathering',
+    '3prot' : 'The protestors are restless and numerous',
+    '3pigs' : 'The police are here to save the day',
+    '3viol' : 'The hooligans are being kept in line',
+    '3riot' : 'Rioters block the streets in business district',
+    '3cage' : 'Violent protestors met by peaceful police',
+    '1prot' : 'These guys are just sitting around',
+    '1pigs' : 'The police dont know what they are doing',
+    '1viol' : 'Unrecognized protestors are being violent',
+    '1riot' : 'Rioters make it unsafe to protest peacefully',
+    '1cage' : 'It feels safe to protest in a free speech zone',
+    '1died' : 'Nobody wants to protest if it isnt nice out',
+    '3died' : 'Nobody wants to protest if it isnt nice out',
+    '_exit' : 'Youve been manipulated. Now it is your turn',
+}
+
 function embed(req, res) {
-        res.sendfile(map[req.params.pgsrc].file, {root : 'content'});
+//      res.sendfile(map[req.params.pgsrc].file, {root : 'content'});
+	fs.readFile('./content/' + map[req.params.pgsrc].file, function (err, data) {
+		if (err) return res.json({error : err});
+                var string = "" + data;
+		res.write(string.replace("--REPLACE--", narrative[req.session.page]));
+                res.end();
+        });
 }
 
 function retrieve(req, res) {
         var s = req.session;
 
+        if (s.page == null) s.page = 'start';
+        if (s.page == "start" || s.page == "_exit") return res.sendfile(map[s.page].file, {root : 'content'});
+
         var html = "<html><body><iframe src=\"node/";
-        if (s.page == null) s.page = "dispa";
-        html += s.page + "\"/>";
-
-        /*
-        TODO: Add a <div> for form etc..
-        */
-
+        if (s.page == null) s.page = "start";
+        html += s.page + "\" width=\"100%\" height=\"80%\" frameBorder=\"0\"></iframe>";
+	html += "<div style=\"position : absolute; bottom : 0; left : 0\"><form name=\"morgan\" action=\"/\" method=\"POST\">";
+        html += "<input type=\"radio\" name=\"choice\" value=\"0\">" + map[s.page]['zed'] + "<br>";
+        html += "<input type=\"radio\" name=\"choice\" value=\"1\">" + map[s.page]['one'] + "<br>";
+        html += "<input type=\"radio\" name=\"choice\" value=\"2\">" + map[s.page]['two'] + "<br>";
+        html += "<input type=\"submit\" name=\"submit\" value=\"submit\">";
+        html += "</form></div>";
         html += "</body></html>";
 
         res.write(html);
@@ -64,6 +98,10 @@ function process(req, res) {
 
                 s.choices.push(c);
                 s.page = map[s.page][c];
+        } else if (s.page == 'start') {
+        	req.session.email = b.email;
+        	req.session.phone = b.phone;
+                s.page = map[s.page][0];
         }
 
 	return res.redirect('/');
@@ -74,16 +112,3 @@ app.get ('/node/:pgsrc', embed);
 app.post('/', process);
 
 module.exports = app;
-
-/*------------------------------------------
-
-
-app.get('/facebook', function (req, res) {
-    res.sendfile('Facebook.html', {root : 'content'});
-});
-
-app.get('/fake', function (req, res) {
-  res.json({message : "This is a fake message"});
-});
-
-------------------------------------------*/
